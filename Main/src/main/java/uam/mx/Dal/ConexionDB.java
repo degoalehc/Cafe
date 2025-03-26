deb6ccead59b19b90f19399354605693e8cc3531
degoalehc <diego.hernandez@cua.uam.mx>
2025-03-26 14:56:01 -0600

    archivo de propiedades y conexion a la base de datos

---
+++ Main/src/main/java/uam/mx/Dal/ConexionDB.java
@@ -0,0 +1,38 @@
+package uam.mx.dal;
+
+import java.awt.image.DataBuffer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class ConexionDB {
+    private String database;
+    private String host;
+    private String port;
+    private String scheme;
+    private String user;
+    private String pass;
+
+    private Connection connection;
+
+    public ConexionDB(){
+        accesoDB();
+    }
+
+    pivate void accesoDB(){
+        Properties properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(DataBuffer.properties)){
+            if(input == null){
+                System.out.println("Archivo db.properties no encontrado");
+            }
+            properties.load(input);
+            database = properties.getProperty("database");
+            host = properties.getProperty("user");
+        }
+    }
+
+}
