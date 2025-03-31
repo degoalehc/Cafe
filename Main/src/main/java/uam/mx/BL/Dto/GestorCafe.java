8f1931642f5a7d5ca70bddc25f6c843fe295136f
degoalehc <diego.hernandez@cua.uam.mx>
2025-03-31 17:00:22 -0600

    Gestor de Cafe

--- Main/src/main/java/uam/mx/BL/Dto/GestorCafe.java
+++ Main/src/main/java/uam/mx/BL/Dto/GestorCafe.java
@@ -1,11 +1,14 @@
-<<<<<<< HEAD
+package uam.mx.bl.dto;
 
-import uam.mx.bl.dto.CafeDto;
-=======
-package uam.mx.BL.dto
-import uam.mx.BL.Dto.CafeDto;
->>>>>>> 446a83dc0842d3b511d0bb2752a00ad1cb9515c3
+import uam.mx.dal.CafeDao;
+import uam.mx.dal.entities.Cafe;
 
 public class GestorCafe {
+    CafeDao cafeteria = new CafeDao();
+    
+    public CafeDto agregarCafe(Cafe cafe){
+
+        return new CafeDto();
+    }
 
 }
